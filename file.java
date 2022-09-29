4441a9ea20cd719fcb6854935883ba062067e611
des <dnsuaws@gmail>
2022-09-29 16:54:04 -0700

    checked on the attention of the students

--- file.java
+++ file.java
@@ -6,3 +6,5 @@ let me work on thi code again
 I will do a push and a pull
 working on some coding this evening Sunday September 25
 today is the 29th of September 2022
+pleas can we get a better understing the fetch ?
+I hope you all are attentive.
